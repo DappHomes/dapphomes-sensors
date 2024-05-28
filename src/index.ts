07925fb48f782d7751df72c0c916f165e11e12ec
Pepe Guerrero <jjguerreromontero@gmail.com>
2024-05-29 01:05:59 +0200

    Change sensor reading approach to reactive
    With this approach we await sensor readings in a http endpoint instead of polling the sensors, which will enable deep-sleep in the sensors.

--- src/index.ts
+++ src/index.ts
@@ -1,56 +1,60 @@
-import { config } from 'dotenv';
+import dotenv from 'dotenv';
 import { Signer, ethers } from 'ethers';
 import { failWith } from './utils';
-import { SensorData, SensorDataProvider } from './sensors/sensorDataProvider';
-import { FakeSensor } from './sensors/fakeSensor';
 import { DataEncryptor } from './encryption/dataEncryptor';
 import { TacoEncryptor } from './encryption/taco';
 import { isSubscribed } from './encryption/tacoConditions';
 import { IpfsStorage } from './storage/storage';
 import { PinataStorage } from './storage/pinata';
+import express, { Express, Request, Response } from 'express';
+import { IpFilter } from 'express-ipfilter';
 
-const setup = async () => {
-    config();
-    const homeUUID: string = process.env.HOME_UUID
-        ?? failWith("Must provide a home uuid in the env file");
-    const rpcProvider: string = process.env.RPC_PROVIDER
-        ?? failWith("Must provide a RPC provider in the env file");
-    const privateKey: string = process.env.PRIV_KEY
-        ?? failWith("Must provide the signer's private key in the env file");
-    const pinataKey: string = process.env.PINATA_JWT_KEY
-        ?? failWith("Must provide Pinata JWT key in the env file");
-    const openWeatherKey: string = process.env.OPEN_WEATHER_API_KEY
-        ?? failWith("Must provide OpenWeather API key for fake sensor in the env file");
-    const provider = new ethers.providers.JsonRpcProvider(rpcProvider);
-    const signer: Signer = new ethers.Wallet(privateKey, provider);
+dotenv.config();
 
-    const sensorDataProvider: SensorDataProvider = new FakeSensor('Malaga', openWeatherKey);
+const homeUUID: string = process.env.HOME_UUID ?? failWith("Must provide a home uuid in the env file");
 
-    const encryptor: DataEncryptor = new TacoEncryptor(provider, signer, isSubscribed);
+const rpcProvider: string = process.env.RPC_PROVIDER ?? failWith("Must provide a RPC provider in the env file");
+const privateKey: string = process.env.PRIV_KEY ?? failWith("Must provide the signer's private key in the env file");
+const provider = new ethers.providers.JsonRpcProvider(rpcProvider);
+const signer: Signer = new ethers.Wallet(privateKey, provider);
+const encryptor: DataEncryptor = new TacoEncryptor(provider, signer, isSubscribed);
 
-    const ipfsStorage: IpfsStorage = new PinataStorage(pinataKey);
+const pinataKey: string = process.env.PINATA_JWT_KEY ?? failWith("Must provide Pinata JWT key in the env file");
+const ipfsStorage: IpfsStorage = new PinataStorage(pinataKey);
 
-    return { homeUUID, sensorDataProvider, encryptor, ipfsStorage };
-};
+const app: Express = express();
+const port = 3000;
+const localhost = '127.0.0.1';
 
-const run = async () => {
-    console.log("Starting setup...");
-    const { homeUUID, sensorDataProvider, encryptor, ipfsStorage } = await setup();
-    console.log("Setup completed");
+app.use(express.json());
 
-    setInterval(() => {
-        sensorDataProvider.getSensorData()
-            .then((sensorData: SensorData) => {
-                console.log(sensorData);
-                return encryptor.encrypt(JSON.stringify(sensorData));
-            })
-            .then((bytes: Uint8Array) => {
-                console.log(bytes);
-                return ipfsStorage.storeCyphertext(homeUUID, bytes);
-            })
-            .then((result) => console.log(result));
-    }, 10000);
-};
+// allow connections only from local network
+const localNetworkIpRanges = [
+    '127.0.0.1',       // localhost
+    '192.168.0.0/16',  // Local Range 192.168.x.x
+    '10.0.0.0/8',      // Local Range 10.x.x.x
+    '172.16.0.0/12'    // Local Range 172.16.x.x to 172.31.x.x
+  ];
 
-run();
+app.use(IpFilter(localNetworkIpRanges, { mode: 'allow' }));
+
+app.post('/sensor-reading/:sensorId', async (req: Request, res: Response) => {
+    try {
+        const { sensorId } = req.params;
+        const sensorData = req.body;
+        // TODO validate request body
+        console.log(`Received data from sensor ${sensorId}:`, sensorData);
+
+        const bytes: Uint8Array = await encryptor.encrypt(JSON.stringify(sensorData));
+        await ipfsStorage.storeCyphertext(homeUUID, bytes);
+
+        res.status(200).json({ message: 'Sensor data received', sensorId, sensorData });
+    } catch (err) {
+        res.status(500).json({ error_message: 'Internal error: ', err })
+    }
+});
+
+app.listen(port, localhost, () => {
+    console.log(`Waiting for readings at http://${localhost}:${port}/`);
+});
 
